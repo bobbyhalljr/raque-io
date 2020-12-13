aa54e467da5b748ed0b942718252dab84ddea7cf
Bobby Hall Jr <bobbyhalljr@Bobbys-MacBook.local>
2020-12-13 13:12:59 -0500

    styling fix in office section

--- src/sections/offices.js
+++ src/sections/offices.js
@@ -46,7 +46,7 @@ const IconContainer = styled.div`
     font-size: 2rem;
     border-radius: ${theme.borderRadius.circle};
     background: #bae1ff;
-    padding: 1.5rem;
+    padding: 1.25rem;
     margin-bottom: 1rem;
     color: ${theme.color.primaryBlue};
     box-shadow: ${theme.boxShadow.cardSmall};
